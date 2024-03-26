df6bd7c748aec32493037f95b8fc04bf4ebff970
Noemi Calizaya <calizayanoemiisabel@gmail.com>
2024-03-26 16:14:14 -0400

    Se agrega router, links al menú y la configuración inicial de TailwindCSS

---
+++ src/components/molecules/header/MainMenu.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const MainMenu = () => {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/">Inicio</Link>
+            </li>
+            <li>
+                <Link to="/productos">Productos</Link>
+            </li>
+            <li>Ofertas</li>
+            <li>Contactos</li>
+        </ul>
+    </nav>
+  )
+}
---
+++ src/components/organisms/MainHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Logo } from '../molecules/header/Logo'
+import { MainMenu } from '../molecules/header/MainMenu'
+
+export const MainHeader = () => {
+  return (
+    <div>
+        <Logo />
+        <MainMenu />
+    </div>
+  )
+}
